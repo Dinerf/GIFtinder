89f7310ef16ba5163c7c6b8b53306dbd94d69211
Fernanda Gava <nanda_gava@hotmail.com>
2018-10-04 10:23:36 -0300

    tela login, key-auth firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAUauAREk0n-Zde_c7NFqAIYU2PQxl6jOg",
+    authDomain: "giftinder-lab.firebaseapp.com",
+    databaseURL: "https://giftinder-lab.firebaseio.com",
+    projectId: "giftinder-lab",
+    storageBucket: "",
+    messagingSenderId: "567013199766"
+  };
+  firebase.initializeApp(config);
---
+++ login.js
@@ -0,0 +1,78 @@
+// BANCO DE DADOS
+const database = firebase.database();
+const USER_ID = localStorage.getItem("userKey");
+
+// if (USER_ID) {
+//   window.location = "screen1.html";
+// }
+
+$(document).ready(function() {
+  $("#sign-in-button").click(clickSignIn);
+  $("#sign-up-button").click(clickSignUp);
+  // splash();
+});
+
+// TELA SPLASH
+function splash() {
+  $(".splash").delay(3000).fadeOut('slow');
+  $(".initial-page").hide();
+  $(".initial-page").delay(3000).fadeIn('slow');
+}
+
+// ENTRAR
+function clickSignIn(event) {
+  event.preventDefault();
+  const email = $("#sign-in-email").val();
+  const password = $("#sign-in-password").val();
+  signIn(email, password);
+}
+
+function signIn(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((response) => {
+      const userID = response.user.uid;
+      sendIdToLocalStorage(userID);
+      window.location = "main.html";
+    })
+    .catch((error) => {
+      handleErrors(error);
+    });
+}
+
+// CADASTRAR
+function clickSignUp(event) {
+  event.preventDefault();
+  const name = $("#sign-up-name").val();
+  const email = $("#sign-up-email").val();
+  const password = $("#sign-up-password").val();
+  signUp(name, email, password)
+};
+
+function signUp(name, email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((response) => {
+      const userID = response.user.uid;
+
+      database.ref("/users/" + userID).set({
+        name: name
+      });
+
+      sendIdToLocalStorage(userID);
+      window.location = "screen1.html";
+    })
+    .catch((error) => {
+      handleErrors(error);
+    });
+}
+
+// ERROS
+function handleErrors(error) {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  console.log(errorCode, errorMessage);
+}
+
+// ENVIAR CHAVE
+function sendIdToLocalStorage(key) {
+  localStorage.setItem("userKey", key);
+}
